67788661818616460c2a669bde069c2a7f292d33
Nat-1405 <nguyenanhthi066@gmail.com>
2026-01-18 17:33:52 +0700

    device-types (category)

--- dtos/device.ts
+++ dtos/device.ts
@@ -32,3 +32,21 @@ export interface DeviceResponse {
     description?: string;
     warrantyExpiryDate?: string;
 }
+
+// --- DEVICE CATEGORY ---
+export interface CreateDeviceCategoryRequest {
+    categoryName: string;
+    description?: string;
+}
+
+export interface UpdateDeviceCategoryRequest {
+    categoryName?: string;
+    description?: string;
+}
+
+export interface DeviceCategoryResponse {
+    categoryId: string;
+    categoryName: string;
+    description?: string;
+    deviceCount?: number;
+}
